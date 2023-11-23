4650ba68bbc11080a802cdc1c884a9bbcf6e036c
hanumoka <amagramer@gmail.com>
2023-11-23 12:22:32 +0900

    tailwind.config.ts 다크모드 설정 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,35 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
+  mode: "jit",
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
+
+// module.exports = {
+//   mode: 'jit',
+//   content: [
+//       './pages/**/*.{js,ts,jsx,tsx}',
+//       './components/**/*.{js,ts,jsx,tsx}',
+//   ],
+//   darkMode: 'class',
+//   theme: {
+//       extend: {},
+//   },
+//   plugins: [],
+// }
