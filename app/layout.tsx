571eddc9d3b222997b5c24c940a930d74b655c49
hanumoka <amagramer@gmail.com>
2023-11-23 10:35:51 +0900

    second commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "hanumoka portfolio",
-  description: "집에 가고 싶다.",
+  description: "집에 가고 싶다1.",
 };
 
 export default function RootLayout({
