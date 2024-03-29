7280607242ecc5d559abb5e691dfcc572794963f
hanumoka <amagramer@gmail.com>
2024-03-29 23:06:56 +0900

    1

--- app/_components/home/hero.tsx
+++ app/_components/home/hero.tsx
@@ -7,8 +7,6 @@ export default function Hero() {
       <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
         <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
           안녕하세요 hanumoka 입니다.
-          <br className="hidden lg:inline-block" />
-          사수는 빨리 나한테 말을 놓아라
         </h1>
         <p className="mb-8 leading-relaxed">집에 가고 싶다.</p>
         <div className="flex justify-center">
